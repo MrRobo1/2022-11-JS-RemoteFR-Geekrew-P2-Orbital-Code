f538f8d53783b5601fc408bd6975c666cd96d293
fturello <92311869+fturello@users.noreply.github.com>
2023-01-19 15:50:03 +0100

    comments removed

--- frontend/src/components/Planet.jsx
+++ frontend/src/components/Planet.jsx
@@ -1,28 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-// import { useNavigate } from "react-router-dom";
-
 import styles from "../styles/Planet.module.css";
 
 function Planet({ planetDetail }) {
-  // console.log("planetDetail ==", planetDetail);
-  // const navigate = useNavigate();
-
-  // const onPressPlanet = () => {
-  //   const path = `/planetdetail`;
-  //   navigate(path);
-  // };
-
   return (
     <div
       className={`${styles.Planet} ${
         planetDetail ? styles["planet-detail"] : ""
       }`}
-      // onClick={onPressPlanet}
-      // onKeyDown={onPressPlanet}
-      // role="button"
-      // tabIndex="0"
     >
       {" "}
     </div>
